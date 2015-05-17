e9869fff23a141dfc976918707d912b29f8a16d9
Bartosz Hejman <bartoszhejman@gmail.com>
2015-05-17 18:06:18 +0200

    Add Capistrano

---
+++ config/deploy/staging.rb
@@ -0,0 +1,13 @@
+role :app, %w{himen15@162.144.12.195:2222}
+role :web, %w{himen15@162.144.12.195:2222}
+role :db,  %w{himen15@162.144.12.195:2222}
+
+set :ssh_options, {
+  keys: %w(/Users/hejmaniok/.ssh/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(publickey password)
+}
+
+set :tmp_dir, "/home1/himen15/tmp"
+
+set :branch, :dev
